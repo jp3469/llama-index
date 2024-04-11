a9d9f28fb75760b580a29e1cc759f84b7d1c2518
Gus Nguyen <gusnguyen@stripe.com>
2024-04-11 15:26:53 -0700

    Added react frontend, backend, and Dockerfile

--- food_flask_app.py
+++ food_flask_app.py
@@ -1,137 +1,33 @@
-from flask import Flask
+import os
+from multiprocessing.managers import BaseManager
+from flask import Flask, request, jsonify, make_response
+from flask_cors import CORS
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+CORS(app)
 
-import os
-import openai
-import nest_asyncio
+# initialize manager connection
+# NOTE: you might want to handle the password in a less hardcoded way
+manager = BaseManager(('', 5602), b'password')
+manager.register('query_agent')
+manager.connect()
 
-from llama_index.core import (
-    SimpleDirectoryReader,
-    VectorStoreIndex,
-    StorageContext,
-)
-from llama_index.core import load_index_from_storage
-from llama_index.readers.file import UnstructuredReader
-from pathlib import Path
-from llama_index.core import VectorStoreIndex, StorageContext
-from llama_index.core import Settings
-from llama_index.core import load_index_from_storage
-from llama_index.core.tools import QueryEngineTool, ToolMetadata
-from llama_index.llms.openai import OpenAI
-from llama_index.core.query_engine import SubQuestionQueryEngine
-from llama_index.agent.openai import OpenAIAgent
-from typing import Sequence, List
-from dotenv import load_dotenv
-from llama_index.llms.openai import OpenAI
-from llama_index.core.llms import ChatMessage
-from llama_index.core.tools import BaseTool, FunctionTool
-
-import nest_asyncio
-import requests
-from llama_index.core import (
-    SimpleDirectoryReader,
-    VectorStoreIndex,
-    StorageContext,
-    load_index_from_storage,
-)
-
-from llama_index.core.tools import QueryEngineTool, ToolMetadata
-from llama_index.core.storage.chat_store import SimpleChatStore
-from llama_index.core.memory import ChatMemoryBuffer
-from llama_index.agent.openai import OpenAIAgent
-from llama_index.llms.openai import OpenAI
-from yelpapi import YelpAPI
-
-import googlemaps
-
-load_dotenv()
-# NOTE: for local testing only, do NOT deploy with your key hardcoded
-GOOGLE_MAPS_API_KEY = os.environ.get("GOOGLE_MAPS_API_KEY")
-yelp_api_key = os.environ.get("YELP_API_KEY")
-
-#index = None
-index_name = "./saved_index"
-
-# Define our tools
-# #1 tool, get top 10 restaurants in a location based on a query
-def restaurant_search(
-    query: str,
-    location: str,
-    api_key: str,
-    limit: int = 5,
-    sort_by: str = "rating",
-) -> List[dict]:
-    yelp_api = YelpAPI(yelp_api_key)
-    response = yelp_api.search_query(
-        term=query, location=location, sort_by=sort_by, limit=limit
-    )
-    return response["businesses"]
-
-# #2 tool, given a business id or alias, get the details of a restaurant
-def restaurant_details_search(
-    business_id_or_alias: str,
-) -> List[dict]:
-    yelp_api = YelpAPI(yelp_api_key)
-    response = yelp_api.business_query(id=business_id_or_alias)
-    return response
-
-# #3 tool, given the location of a restaurant, get the distance and travel time to and from your current location to the restaurant
-def distance_to_restaurant(
-        curr_location: str,
-        restaurant_location: str
-    ):
-        map_client = googlemaps.Client(GOOGLE_MAPS_API_KEY)
-        response = map_client.distance_matrix(destinations = restaurant_location, origins = curr_location)
-        return response
-
-def initialize_index():
-    global agent
-    restaurant_search_tool = FunctionTool.from_defaults(fn=restaurant_search)
-    restaurant_details_tool = FunctionTool.from_defaults(fn=restaurant_details_search)
-    distance_to_restaurant_tool = FunctionTool.from_defaults(fn=distance_to_restaurant)
-
-    # Define our chat store
-    # chat_store = SimpleChatStore.from_persist_path(
-    #     persist_path="chat_store.json"
-    # )
-    chat_store = SimpleChatStore()
-
-    chat_memory = ChatMemoryBuffer.from_defaults(
-        token_limit=10000,
-        chat_store=chat_store,
-        chat_store_key="user1",
-    )
-
-
-    llm = OpenAI(model="gpt-3.5-turbo-1106")
-    agent = OpenAIAgent.from_tools(
-        [restaurant_search_tool, restaurant_details_tool, distance_to_restaurant_tool], llm=llm, memory=chat_memory
-    )
-
-
-initialize_index()
-from flask import request
 
 @app.route("/query", methods=["GET"])
-def query_index():
-    # global index
+def query_agent():
+    global manager
     query_text = request.args.get("text", None)
     if query_text is None:
-        return (
-            "No text found, please include a ?text=blah parameter in the URL",
-            400,
-        )
-    # query_engine = index.as_query_engine()
-    # response = query_engine.query(query_text)
-    response = agent.chat(query_text)
-    return str(response), 200
+        return "No text found, please include a ?text=blah parameter in the URL", 400
+    
+    response = manager.query_agent(query_text)
+    return make_response(jsonify(response)), 200
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return "Hello, World! Welcome to the food chatbot!"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5601)
-
---
+++ index_server.py
@@ -0,0 +1,133 @@
+import os
+import pickle
+
+# NOTE: for local testing only, do NOT deploy with your key hardcoded
+os.environ['OPENAI_API_KEY'] = "your key here"
+
+from multiprocessing import Lock
+from multiprocessing.managers import BaseManager
+from llama_index import SimpleDirectoryReader, GPTVectorStoreIndex, Document, ServiceContext, StorageContext, load_index_from_storage
+
+from llama_index.core import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    StorageContext,
+)
+from llama_index.core import load_index_from_storage
+from llama_index.readers.file import UnstructuredReader
+from pathlib import Path
+from llama_index.core import VectorStoreIndex, StorageContext
+from llama_index.core import Settings
+from llama_index.core import load_index_from_storage
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.llms.openai import OpenAI
+from llama_index.core.query_engine import SubQuestionQueryEngine
+from llama_index.agent.openai import OpenAIAgent
+from typing import Sequence, List
+from dotenv import load_dotenv
+from llama_index.llms.openai import OpenAI
+from llama_index.core.llms import ChatMessage
+from llama_index.core.tools import BaseTool, FunctionTool
+
+import nest_asyncio
+import requests
+from llama_index.core import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    StorageContext,
+    load_index_from_storage,
+)
+
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.storage.chat_store import SimpleChatStore
+from llama_index.core.memory import ChatMemoryBuffer
+from llama_index.agent.openai import OpenAIAgent
+from llama_index.llms.openai import OpenAI
+from yelpapi import YelpAPI
+
+
+index = None
+stored_docs = {}
+lock = Lock()
+
+index_name = "./saved_index"
+pkl_name = "stored_documents.pkl"
+
+# Define our tools
+# #1 tool, get top 10 restaurants in a location based on a query
+def restaurant_search(
+    query: str,
+    location: str,
+    api_key: str,
+    limit: int = 5,
+    sort_by: str = "rating",
+) -> List[dict]:
+    yelp_api = YelpAPI(yelp_api_key)
+    response = yelp_api.search_query(
+        term=query, location=location, sort_by=sort_by, limit=limit
+    )
+    return response["businesses"]
+
+# #2 tool, given a business id or alias, get the details of a restaurant
+def restaurant_details_search(
+    business_id_or_alias: str,
+) -> List[dict]:
+    yelp_api = YelpAPI(yelp_api_key)
+    response = yelp_api.business_query(id=business_id_or_alias)
+    return response
+
+# #3 tool, given the location of a restaurant, get the distance and travel time to and from your current location to the restaurant
+def distance_to_restaurant(
+        curr_location: str,
+        restaurant_location: str
+    ):
+        map_client = googlemaps.Client(GOOGLE_MAPS_API_KEY)
+        response = map_client.distance_matrix(destinations = restaurant_location, origins = curr_location)
+        return response
+
+def initialize_agent():
+    """Create a new global index, or load one from the pre-set path."""
+    global agent
+    restaurant_search_tool = FunctionTool.from_defaults(fn=restaurant_search)
+    restaurant_details_tool = FunctionTool.from_defaults(fn=restaurant_details_search)
+    distance_to_restaurant_tool = FunctionTool.from_defaults(fn=distance_to_restaurant)
+
+    # Define our chat store
+    # chat_store = SimpleChatStore.from_persist_path(
+    #     persist_path="chat_store.json"
+    # )
+    chat_store = SimpleChatStore()
+
+    chat_memory = ChatMemoryBuffer.from_defaults(
+        token_limit=10000,
+        chat_store=chat_store,
+        chat_store_key="user1",
+    )
+
+
+    llm = OpenAI(model="gpt-3.5-turbo-1106")
+    agent = OpenAIAgent.from_tools(
+        [restaurant_search_tool, restaurant_details_tool, distance_to_restaurant_tool], llm=llm, memory=chat_memory
+    )
+
+
+def query_agent(query_text):
+    """Ask the agent a question."""
+    global agent
+    response = agent.chat(query_text)
+    return response
+
+
+if __name__ == "__main__":
+    # init the global index
+    print("initializing index...")
+    initialize_index()
+
+    # setup server
+    # NOTE: you might want to handle the password in a less hardcoded way
+    manager = BaseManager(('', 5602), b'password')
+    manager.register('query_agent', query_agent)
+    server = manager.get_server()
+
+    print("server started...")
+    server.serve_forever()
